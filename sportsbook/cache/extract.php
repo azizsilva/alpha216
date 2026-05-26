d971c8f11205ca48d1fe9e9aeebf27b6c88ddf5d
azizrezgui <azizrezgui60@gmail.com>
2026-05-26 12:58:57 +0100

    Fix cache tracking error

--- sportsbook/cache/extract.php
+++
@@ -1,9 +0,0 @@
-<?php
-$j = json_decode(file_get_contents('upcoming_sample.json'), true);
-$leagues = [];
-foreach($j['results'] as $r) {
-    $id = $r['league']['id'];
-    $name = $r['league']['name'];
-    $leagues[$id] = $name;
-}
-foreach($leagues as $id => $name) echo "$id | $name\n";
